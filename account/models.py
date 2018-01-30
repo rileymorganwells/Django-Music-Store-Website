5071f3131709c928d78e4d3c3d16800d9cb2e25e
Riley Wells <rileymorganwells@gmail.com>
2018-01-30 12:39:29 -0700

    added get_purchases method to usermodel, closing usermodel branch

--- account/models.py
+++ account/models.py
@@ -8,6 +8,8 @@ class User(AbstractCUser):
     state = models.TextField(blank=True, null=True)
     zip = models.TextField(blank=True, null=True)
 
+    def get_purchases(self):
+        return [ 'Roku Ultimate 2000', 'USB Cable', 'Candy Bar' ]
 
 # class Question(models.Model):
 #     question_text = models.TextField(blank=True, null=True)

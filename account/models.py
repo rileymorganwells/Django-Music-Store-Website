bdf396cd3adce8d4cc64e2e6795010b4a15bea69
Riley Wells <rileymorganwells@gmail.com>
2018-01-30 15:33:33 -0700

    get_purchases added to models.py andsetup added to test

--- account/models.py
+++ account/models.py
@@ -10,15 +10,3 @@ class User(AbstractCUser):
 
     def get_purchases(self):
         return [ 'Roku Ultimate 2000', 'USB Cable', 'Candy Bar' ]
-
-# class Question(models.Model):
-#     question_text = models.TextField(blank=True, null=True)
-#     pub_date = models.DateTimeField(blank=True, null=True)
-#     minutes = models.IntegerField(null=True)
-#     address = models.TextField(blank=True, null=True)
-#     citystatezip = models.TextField(blank=True, null=True)
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)

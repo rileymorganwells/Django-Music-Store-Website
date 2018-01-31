04f714368c97d5c90a83a050948a34c5f0e9a6b6
Riley Wells <rileymorganwells@gmail.com>
2018-01-31 15:29:31 -0700

    updated test.py and created fixtures file

--- account/tests.py
+++ account/tests.py
@@ -2,7 +2,11 @@ from django.test import TestCase
 from account import models as amod
 
 class UserModelTest(TestCase):
+
+    fixtures = ['data.yaml']
+
     def setUp(self):
+        self.user1 =
         self.u1 = amod.User()
         self.u1.first_name = 'Marge'
         self.u1.last_name = 'Simpson'
@@ -11,6 +15,7 @@ class UserModelTest(TestCase):
         self.u1.set_password('password')
         # ... more fields
         self.u1.save()
+        self.u2 = amod.User.objects.get(email='homer@simpsons.com')
 
     def test_user_create_save_load(self):
         '''Tests round trip of User model data to/from database'''
@@ -23,12 +28,21 @@ class UserModelTest(TestCase):
 
     def test_add_groups_check_permissions(self):
         '''Add groups to a user and check permissions'''
-        a = 1
-        self.assertEqual(a, 1)
+        for p in Permission.objects.all():
+            print(p.content_type.app_label + "." + p.codename)
 
-        # def test_animals_can_speak(self):
-        # """Animals that can speak are correctly identified"""
-        # lion = Animal.objects.get(name="lion")
-        # cat = Animal.objects.get(name="cat")
-        # self.assertEqual(lion.speak(), 'The lion says "roar"')
-        # self.assertEqual(cat.speak(), 'The cat says "meow"')
+        p1 = Permission()
+        p1.name = 'Change product price'
+        p1.codename = 'change_product_price'
+        p1.content_type = ContentType.objects.get(id=1)
+        p1.save()
+        self.u1.user_permissions.add(p1)
+
+        g1 = Group()
+        g1.name = 'Salespeople'
+        g1.save()
+        g1.permissions.add(g1)
+        g1.save()
+        self.u1.groups.add(g1)
+
+        self.assertTrue(self.u1.has_perm('auth.delete_group'))

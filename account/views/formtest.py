e066efc4585f8e8c4a089f368bd65f51c62ab00f
Riley Wells <rileymorganwells@gmail.com>
2018-02-06 14:14:11 -0700

    form validations added

--- account/views/formtest.py
+++ account/views/formtest.py
@@ -1,18 +1,18 @@
 from django_mako_plus import view_function
 from django import forms
 from django.http import HttpResponseRedirect
+from formlib.form import Formless
 
 @view_function
 def process_request(request):
-    # process the form
-    if request.method == 'POST':
-        form = TestForm(request.POST)
-        if form.is_valid():
-            # work of the form - create user, login user, purchase
-            return HttpResponseRedirect('/')
 
-    else:
-        form = TestForm()
+    form = TestForm(request)
+    if form.is_valid():
+        # once you get here, you cannet yell at the user anymore
+        # this area is for an absolutely perfect form
+        # ready to be processed
+        # work of the form - create user, login user, purchase
+        return HttpResponseRedirect('/')
 
     # render the form
     context = {
@@ -20,6 +20,37 @@ def process_request(request):
     }
     return request.dmp_render('formtest.html', context)
 
-class TestForm(forms.Form):
-    comment = forms.CharField(label='Your comment')
-    renewal_date = forms.DateField(help_text="")
+class TestForm(Formless):
+
+    def init(self):
+        self.fields['email'] = forms.EmailField(label='Email')
+        self.fields['Age'] = forms.IntegerField(label='Age', min_value=18)
+        self.fields['renewal_date'] = forms.DateField(help_text="")
+        self.fields['password'] = forms.CharField(max_length=32, widget=forms.PasswordInput, label='Password')
+        self.fields['password2'] = forms.CharField(max_length=32, widget=forms.PasswordInput, label='Confirm Password')
+
+    # def clean_email(self):
+    #     un = self.cleaned_data.get('email')
+    #
+    #     # check if this email already exists (objects.get thing)
+    #     if len(users) > 0:
+    #         raise forms.ValidationError('Thie email already exists')
+    #
+    #     return un
+
+    def clean_age(self):
+        age = self.cleaned_data.get('age')
+        if age < 18:
+            # show an error message: no soup for you
+            print('>>>>>> LESS THAN 18')
+        return age
+
+    def clean(self):
+        # for checking the entire form - usually two variables at once
+        # "password" and "password2"
+        pw1 = self.cleaned_data.get('password')
+        pw2 = self.cleaned_data.get('password2')
+        if pw1 != pw2:
+            # yell at the user
+            print('>>>>>> Passwords don\'t match!')
+        return self.cleaned_data

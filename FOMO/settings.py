efe6baff8c41efe02bf311fe5c559e049ef5a8fb
Riley Wells <rileymorganwells@gmail.com>
2018-02-12 14:56:25 -0700

    changedettings to add postgres

--- FOMO/settings.py
+++ FOMO/settings.py
@@ -155,8 +155,12 @@ WSGI_APPLICATION = 'FOMO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fomo',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

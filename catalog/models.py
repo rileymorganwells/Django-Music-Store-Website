b3d0048aac1415daa37b12caacfd065a8362537e
Riley Wells <rileymorganwells@gmail.com>
2018-02-13 12:40:32 -0700

    deleted extra files in project

--- catalog/models.py
+++ catalog/models.py
@@ -3,16 +3,16 @@ from django.db import models
 class Product():
     name = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    createdate = models.DateField(blank=True, null=True)
-    lastmodified = models.DateField(blank=True, null=True)
+    createdate = models.DateTimeField(blank=True, null=True)
+    lastmodified = models.DateTimeField(blank=True, null=True)
 
 class BulkProduct():
     name = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     category = models.TextField(blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
-    createdate = models.DateField(blank=True, null=True)
-    lastmodified = models.DateField(blank=True, null=True)
+    createdate = models.DateTimeField(blank=True, null=True)
+    lastmodified = models.DateTimeField(blank=True, null=True)
     quantity = models.IntegerField(blank=True, null=True)
     reordertrigger = models.IntegerField(blank=True, null=True)
     reorderquantity = models.IntegerField(blank=True, null=True)
@@ -22,8 +22,8 @@ class UniqueProduct():
     description = models.TextField(blank=True, null=True)
     category = models.TextField(blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
-    createdate = models.DateField(blank=True, null=True)
-    lastmodified = models.DateField(blank=True, null=True)
+    createdate = models.DateTimeField(blank=True, null=True)
+    lastmodified = models.DateTimeField(blank=True, null=True)
     itemID = models.IntegerField(blank=True, null=True)
 
 class RentalProduct():
@@ -31,10 +31,10 @@ class RentalProduct():
     description = models.TextField(blank=True, null=True)
     category = models.TextField(blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
-    createdate = models.DateField(blank=True, null=True)
-    lastmodified = models.DateField(blank=True, null=True)
+    createdate = models.DateTimeField(blank=True, null=True)
+    lastmodified = models.DateTimeField(blank=True, null=True)
     itemID = models.IntegerField(blank=True, null=True)
     # maxrental = models.
-    retiredate = models.DateField(blank=True, null=True)
+    retiredate = models.DateTimeField(blank=True, null=True)
     # def get_purchases(self):
     #     return [ 'Roku Ultimate 2000', 'USB Cable', 'Candy Bar' ]

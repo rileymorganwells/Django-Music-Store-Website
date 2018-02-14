a6a0b4f6724c81f85d2823cf413401d9eabe5ec9
Riley Wells <rileymorganwells@gmail.com>
2018-02-14 14:59:29 -0700

    sldfkj

--- catalog/models.py
+++ catalog/models.py
@@ -7,17 +7,19 @@ class Category():
     lastmodified = models.DateTimeField(blank=True, null=True)
 
 # pip install django-polymorphic
-class Product(PolymorphicModel):
-        # polymorphic_ctype: 10
-        # create_date: ! '2018-02-14 14:41:28.900082'
-        # last_modified: ! '2018-02-14 14:41:28.900095'
-        # status: A
-        # name: Fight Song
-        # description: By Rachel Platten and Dave Bassett
-        # category: 1
-        # price: '5.00'
+# class Product(PolymorphicModel):
+#         # polymorphic_ctype: 10
+#         # create_date: ! '2018-02-14 14:41:28.900082'
+#         # last_modified: ! '2018-02-14 14:41:28.900095'
+#         # status: A
+#         # name: Fight Song
+#         # description: By Rachel Platten and Dave Bassett
+#         # category: 1
+#         # price: '5.00'
 
 class BulkProduct():
+    '''Bulk Product'''
+    TITLE = 'Bulk'
     name = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     category = models.TextField(blank=True, null=True)
@@ -27,8 +29,11 @@ class BulkProduct():
     quantity = models.IntegerField(blank=True, null=True)
     reordertrigger = models.IntegerField(blank=True, null=True)
     reorderquantity = models.IntegerField(blank=True, null=True)
+        # quantity: 20
+        # reorder_trigger: 10
+        # reorder_quantity: 10
 
-class UniqueProduct():
+class IndividualProduct():
     name = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     category = models.TextField(blank=True, null=True)
@@ -36,6 +41,7 @@ class UniqueProduct():
     createdate = models.DateTimeField(blank=True, null=True)
     lastmodified = models.DateTimeField(blank=True, null=True)
     itemID = models.IntegerField(blank=True, null=True)
+        # pid: 12345X6
 
 class RentalProduct():
     name = models.TextField(blank=True, null=True)
@@ -45,7 +51,7 @@ class RentalProduct():
     createdate = models.DateTimeField(blank=True, null=True)
     lastmodified = models.DateTimeField(blank=True, null=True)
     itemID = models.IntegerField(blank=True, null=True)
-    # maxrental = models.
     retiredate = models.DateTimeField(blank=True, null=True)
-    # def get_purchases(self):
-    #     return [ 'Roku Ultimate 2000', 'USB Cable', 'Candy Bar' ]
+        # pid: MACK12A
+        # max_rental_days: 2
+        # retire_date: null

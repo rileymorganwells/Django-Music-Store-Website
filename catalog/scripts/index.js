9df8f75b54f72af154278579599c33c3e9f0e49f
Riley Wells <rileymorganwells@gmail.com>
2018-03-06 13:59:13 -0700

    lots of catalog updates

--- catalog/scripts/index.js
+++ catalog/scripts/index.js
@@ -1,6 +1,12 @@
 (function(context) {
-    
     // utc_epoch comes from index.py
     console.log('Current epoch in UTC is ' + context.utc_epoch);
-    
+    return function() {
+      //need 3 functions - page_load
+      //  -left button
+      //  -right button $("#prevpage"). click(function() {
+      //            $("#products").load("/catalog/index.products/" + context.cid + pagenum)})
+
+   }
+
 })(DMP_CONTEXT.get());
--- catalog/views/index.py
+++ catalog/views/index.py
@@ -2,13 +2,28 @@ from django.conf import settings
 from django_mako_plus import view_function, jscontext
 from catalog import models as cmod
 from catalog.models import Category
+import math
 
 @view_function
-def process_request(request, selection:cmod.Category):
+def process_request(request, selection:cmod.Category=None):
     categories = cmod.Category.objects.all()
+    products = cmod.Product.objects.all()
+    if selection is not None:
+        products = products.filter(category=selection)
     context = {
         'category': categories,
         'selection': selection,
-        'num_pages': 5,
+        'num_pages': math.ceil(products.count()/6),
+        'product': products[2],
+        #jscontext('cid'): selection.id,
     }
     return request.dmp.render('index.html', context)
+
+@view_function
+def products(request, selection:cmod.Category=None, pnum:int=1):
+    #do work
+    products = cmod.Product.objects.all()
+    if selection is not None:
+        products = products.filter(category=selection)
+    products = products[(pnum-1)*6:pnum*6]
+    return request.dmp.render('index.products.html', products)
